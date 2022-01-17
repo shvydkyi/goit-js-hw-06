8b94006d63d197b56a2589bcf450f2f62e0bbc53
shvydkyi <shvydkyi@gmail.com>
2022-01-17 23:00:46 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,8 @@
+const elemRef = document.querySelectorAll('.item');
+const countNumberOfCategories = elemRef.length;
+console.log(` Number of categories: ${countNumberOfCategories}`);
 
+elemRef.forEach(element => {
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Category: ${element.lastElementChild.children.length}`);
+});
