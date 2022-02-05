9f52df90ca1199e9a4b0719a652d8caffcd634b6
shvydkyi <shvydkyi@gmail.com>
2022-02-05 15:46:22 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulRef = document.querySelector("#ingredients");
+const listOfIngredients = ingredients.map((ingredient) => {
+    const itemLi = document.createElement("li");
+    itemLi.textContent = ingredient;
+    itemLi.classList.add("item");
+
+    console.log(itemLi);
+    return itemLi;
+});
+
+ulRef.append(...listOfIngredients)
+console.log(listOfIngredients);
